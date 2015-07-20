53ec23e8fe98d1afc0bc27ca6666b8c878552196
jomoro1988 <jonathan.moreau@usherbrooke.ca>
2015-07-20 09:15:58 -0400

    Added package

--- float_complex_pkg.vhd
+++ float_complex_pkg.vhd
@@ -6,4 +6,11 @@
 -- Date       : $Date: 2015-07-20 $
 -- --------------------------------------------------------------------
 
-Test the commit
+use STD.TEXTIO.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+use ieee.fixed_float_types.all;
+use ieee.fixed_pkg.all;
+
+package float_complex_pkg is
