d0263f3718131a81f3efed7143de29b4e7313b82
jomoro1988 <jonathan.moreau@usherbrooke.ca>
2015-07-21 10:36:55 -0400

    Added arithmetic and comparaison function prototypes

--- float_complex_pkg.vhd
+++ float_complex_pkg.vhd
@@ -2,6 +2,7 @@
 -- "float_complex_pkg" package contains functions for complex floating point math.
 --
 -- --------------------------------------------------------------------
+-- Prototypes of float_complex_pkg functions
 -- Version    : $Revision: 0.1 $
 -- Date       : $Date: 2015-07-20 $
 -- --------------------------------------------------------------------
@@ -15,6 +16,7 @@ use ieee.fixed_pkg.all;
 
 package float_complex_pkg is
 
+  -- Signal description
   constant cplx_sign_width     : NATURAL    := 1;
   constant cplx_exponent_width : NATURAL    := 5;
   constant cplx_fraction_width : NATURAL    := 10;
@@ -22,14 +24,32 @@ package float_complex_pkg is
   constant cplx_imag_width : NATURAL    := 16;
   constant cplx_real_width : NATURAL    := 16;
 
-  -- Author David Bishop (dbishop@vhdl.org)
-
-  -- Note that the size of the vector is not defined here, but in
-  -- the package which calls this one.
-  type UNRESOLVED_complex is array (INTEGER range <>) of STD_ULOGIC;  -- main type
+  type UNRESOLVED_complex is array (0 to 31) of STD_ULOGIC;  -- main type
   subtype U_complex is UNRESOLVED_complex;
 
   subtype complex is UNRESOLVED_complex;
+
+  -- Arithmetic function
+  function add(      l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
+  function subtract( l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
+  function multiply( l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
+  function divide(   l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
+  function divideby2( arg : UNRESOLVED_complex) return UNRESOLVED_complex;
+  function conjugate( arg : UNRESOLVED_complex) return UNRESOLVED_complex;
+
+  -- Comparaison functions
+  function ne(l, r : UNRESOLVED_complex) return boolean;
+  function eq(l, r : UNRESOLVED_complex) return boolean;
+  function gt(l, r : UNRESOLVED_complex) return boolean;
+  function lt(l, r : UNRESOLVED_complex) return boolean;
+  function lte(l, r : UNRESOLVED_complex) return boolean;
+  function gte(l, r : UNRESOLVED_complex) return boolean;
+
+-------------------------------------------------------------------------------
+  -- *** Private function ***
+  -- Bit operation 
+-------------------------------------------------------------------------------
+  
 
 end package float_complex_pkg;
 
