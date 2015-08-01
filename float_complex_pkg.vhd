080042a344acfe5034a4dc1f7f27c8ce0b69cac8
jomoro1988 <jonathan.moreau@usherbrooke.ca>
2015-08-01 11:34:59 -0400

    mantissa shift for add

--- float_complex_pkg.vhd
+++ float_complex_pkg.vhd
@@ -98,11 +98,56 @@ function find_leftmost (ARG : UNSIGNED; Y : STD_ULOGIC)
   	i_fract := arg(man_width-1 downto 0);
   end procedure break_number;
 
-  function add (l, r : UNRESOLVED_complex) 
-  	return UNRESOLVED_complex is
-  begin
-  	
-  end function add;
+  	function add (l, r : UNRESOLVED_complex) 
+  		return UNRESOLVED_complex is
+  		variable lr_exp, li_exp, rr_exp, ri_exp, shift_i, shift_r, resr_exp, resi_exp : signed;
+  		variable lr_man, li_man, rr_man, ri_man, resrs_man, resis_man, resrc_man, resic_man, resr_man, resi_man : unsigned;
+  		variable lr_sign, li_sign, rr_sign, ri_sign, resr_sign, resi_sign : STD_ULOGIC;
+  	begin
+  		break_number(
+  			arg => l,
+  			r_fract => lr_man,
+  			i_fract => li_man,
+  			r_expon => lr_exp,
+  			i_expon => li_exp,
+  			r_sign  => lr_sign,
+  			i_sign  => li_sign);
+
+  		break_number(
+  			arg => r,
+  			r_fract => rr_man,
+  			i_fract => ri_man,
+  			r_expon => rr_exp,
+  			i_expon => ri_exp,
+  			r_sign  => rr_sign,
+  			i_sign  => ri_sign);
+
+  		shift_r := lr_exp - rr_exp;
+  		shift_i := li_exp - ri_exp;
+
+  		if shift_r < 0 then
+  			resrs_man := shift_right (lr_man, to_integer(-shift_r));
+  			resrc_man := rr_man;
+  			resr_sign := rr_sign;
+  		elsif shift_r > 0 then
+  			resrs_man := shift_right (rr_man, to_integer(shift_r));
+  			resrc_man := lr_man;
+  			resr_sign := lr_sign;
+  		else
+  			if rr_man > lr_man then
+  				resrs_man := lr_man;
+  				resrc_man := rr_man;
+  				resr_sign := rr_sign;
+  			else 
+  				resrs_man := rr_man;
+  				resrc_man := lr_man;
+  				resr_sign := lr_sign;
+  			end if;
+  		end if;
+
+  		
+
+  	end function add;
 
 
 
