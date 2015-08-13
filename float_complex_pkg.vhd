a3ade823b5315e2a8cb15fcbc2fd929077245f10
jomoro1988 <jonathan.moreau@usherbrooke.ca>
2015-08-13 15:01:48 -0400

    Compiling library

--- float_complex_pkg.vhd
+++ float_complex_pkg.vhd
@@ -32,25 +32,25 @@ package complex_short is
 	subtype complex is UNRESOLVED_complex;
 
 	procedure break_number (              -- internal version
-    arg     : in  UNRESOLVED_complex;
+    input_value     : in  UNRESOLVED_complex;
     r_fract : out UNSIGNED;
-    r_expon : out SIGNED;
+    r_expon : out UNSIGNED;
     r_sign  : out STD_ULOGIC;
     i_fract : out UNSIGNED;
-    i_expon : out SIGNED;
+    i_expon : out UNSIGNED;
     i_sign  : out STD_ULOGIC);
 
   function build_number (real_sfrac, imag_sfrac : signed; real_exp, imag_exp : UNSIGNED) return UNRESOLVED_complex;
 
-	function find_leftmost (ARG : UNSIGNED; Y : STD_ULOGIC) return INTEGER;
+	function find_leftmost (input_value : signed; Y : STD_ULOGIC) return integer;
 
 	function to_complex (arg: std_ulogic_vector(31 downto 0)) return UNRESOLVED_complex;
 
   function negate(arg : UNRESOLVED_complex) return UNRESOLVED_complex;
 
-	function to_sulv (arg : UNRESOLVED_complex) return STD_ULOGIC_VECTOR;
-	alias to_StdULogicVector is to_sulv [UNRESOLVED_complex return STD_ULOGIC_VECTOR];
-	alias to_Std_ULogic_Vector is to_sulv [UNRESOLVED_complex return STD_ULOGIC_VECTOR];
+	--function to_sulv (arg : UNRESOLVED_complex) return STD_ULOGIC_VECTOR;
+	--alias to_StdULogicVector is to_sulv [UNRESOLVED_complex return STD_ULOGIC_VECTOR];
+	--alias to_Std_ULogic_Vector is to_sulv [UNRESOLVED_complex return STD_ULOGIC_VECTOR];
 
 	function add(l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
   function "+" (l, r : UNRESOLVED_complex) return UNRESOLVED_complex;
@@ -62,32 +62,30 @@ end complex_short;
 
 package body complex_short is
 
-function to_sulv (
-    arg : UNRESOLVED_float)             -- fp vector
-    return STD_ULOGIC_VECTOR is
-    variable result : STD_ULOGIC_VECTOR (arg'length-1 downto 0);
-begin  -- function to_std_ulogic_vector
-    result := STD_ULOGIC_VECTOR (arg);
-    return result;
-end function to_sulv;
+--function to_sulv (arg : UNRESOLVED_float) return STD_ULOGIC_VECTOR is
+--    variable result : STD_ULOGIC_VECTOR (arg'length-1 downto 0);
+--begin  -- function to_std_ulogic_vector
+--    result := STD_ULOGIC_VECTOR(arg);
+--    return result;
+--end function to_sulv;
 
 function to_complex (arg : std_ulogic_vector(31 downto 0)) return UNRESOLVED_complex is
 	variable result : UNRESOLVED_complex;
 begin 
 	  result(real_start) := arg(real_start);
     result(imag_start) := arg(imag_start);
-    result(real_start-1 downto real_start-exp_width) := toarg(real_start-1 downto real_start-exp_width);
-    result(imag_start-1 downto imag_start-exp_width) := arg(imag_start-1 downto imag_start-exp_width);
-    result(imag_start + man_width downto imag_start +1) := arg(imag_start + man_width downto imag_start +1);
-    result(man_width-1 downto 0) := arg(man_width-1 downto 0);
+    result(real_start-1 downto real_start-exp_width) := UNRESOLVED_complex(arg(real_start-1 downto real_start-exp_width));
+    result(imag_start-1 downto imag_start-exp_width) := UNRESOLVED_complex(arg(imag_start-1 downto imag_start-exp_width));
+    result(imag_start + man_width downto imag_start +1) := UNRESOLVED_complex(arg(imag_start + man_width downto imag_start +1));
+    result(man_width-1 downto 0) := UNRESOLVED_complex(arg(man_width-1 downto 0));
 	return result;
 end function to_complex;
 
-function find_leftmost (ARG : std_ulogic_vector; Y : STD_ULOGIC)
-    return INTEGER is
+function find_leftmost (input_value : signed; Y : STD_ULOGIC)
+    return integer is
   begin
-    for INDEX in (ARG'left - 1) to 0 loop
-      if ARG(INDEX) = Y then
+    for INDEX in (input_value'left - 1) to 0 loop
+      if input_value(INDEX) = Y then
         return INDEX;
       end if;
     end loop;
@@ -95,47 +93,49 @@ function find_leftmost (ARG : std_ulogic_vector; Y : STD_ULOGIC)
   end function find_leftmost;
 
   procedure break_number (              -- internal version
-    arg     : in  UNRESOLVED_complex;
-    r_fract : out UNSIGNED;
-    r_expon : out SIGNED;
+    input_value    : in  UNRESOLVED_complex;
+    r_fract : out UNSIGNED ;
+    r_expon : out UNSIGNED ;
     r_sign  : out STD_ULOGIC;
-    i_fract : out UNSIGNED;
-    i_expon : out SIGNED;
+    i_fract : out UNSIGNED ;
+    i_expon : out UNSIGNED;
     i_sign  : out STD_ULOGIC) is
   begin
-  	r_sign := arg(real_start);
-  	i_sign := arg(imag_start);
-  	r_expon := to_signed(arg(real_start-1 downto real_start-exp_width), exp_width);
-  	i_expon := arg(imag_start-1 downto imag_start-exp_width);
-  	r_fract := to_unsigned(arg(imag_start + man_width downto imag_start +1), man_width);
-  	i_fract := arg(man_width-1 downto 0);
+  	r_sign := input_value(real_start);
+  	i_sign := input_value(imag_start);
+  	r_expon := unsigned(input_value(real_start-1 downto real_start-exp_width));
+  	i_expon := unsigned(input_value(imag_start-1 downto imag_start-exp_width));
+  	r_fract := unsigned(input_value(imag_start + man_width downto imag_start +1));
+  	i_fract := unsigned(input_value(man_width-1 downto 0));
   end procedure break_number;
 
   function build_number (real_sfrac, imag_sfrac : signed; real_exp, imag_exp : UNSIGNED) 
     return UNRESOLVED_complex is
     variable result : UNRESOLVED_complex;
   begin
-    result(real_start) := real_sfrac'left;
-    result(imag_start) := imag_sfrac'left;
-    result(real_start-1 downto real_start-exp_width) := real_exp;
-    result(imag_start-1 downto imag_start-exp_width) :=imag_exp;
-    result(imag_start + man_width downto imag_start +1) := real_sfrac(real_sfrac'left-1 downto 0);
-    result(man_width-1 downto 0) := imag_sfrac(imag_sfrac'left-1 downto 0);
+    result(real_start) := real_sfrac(real_sfrac'left);
+    result(imag_start) := imag_sfrac(imag_sfrac'left);
+    result(real_start-1 downto real_start-exp_width) := UNRESOLVED_complex(real_exp);
+    result(imag_start-1 downto imag_start-exp_width) := UNRESOLVED_complex(imag_exp);
+    result(imag_start + man_width downto imag_start +1) := UNRESOLVED_complex(real_sfrac(real_sfrac'left-1 downto 0));
+    result(man_width-1 downto 0) := UNRESOLVED_complex(imag_sfrac(imag_sfrac'left-1 downto 0));
     return result;
   end function build_number;
 
   	function add (l, r : UNRESOLVED_complex) 
   		return UNRESOLVED_complex is
   		variable guard_bit : integer := 3;
-  		variable lr_exp, li_exp, rr_exp, ri_exp, shift_i, shift_r, resr_exp, resi_exp, resrt_exp, resit_exp : unsigned;
-  		variable pos_i, pos_r, expr_corr, expi_corr : UNSIGNED;
-  		variable lr_man, li_man, rr_man, ri_man : std_ulogic_vector (man_width-1 downto 0);
+      variable shift_i, shift_r, pos_i, pos_r : integer;
+  		variable lr_exp, li_exp, rr_exp, ri_exp : unsigned;
+      variable resr_exp, resi_exp, resrt_exp, resit_exp : unsigned;
+  		variable expr_corr, expi_corr : UNSIGNED;
+  		variable lr_man, li_man, rr_man, ri_man : unsigned;
   		variable resrs_man, resis_man, resrc_man, resic_man, resr_man, resi_man : signed;
   		variable lr_sign, li_sign, rr_sign, ri_sign, resr_sign, resi_sign : STD_ULOGIC;
       variable result : UNRESOLVED_complex;
   	begin
   		break_number(
-  			arg => l,
+  			input_value => l,
   			r_fract => lr_man,
   			i_fract => li_man,
   			r_expon => lr_exp,
@@ -144,7 +144,7 @@ function find_leftmost (ARG : std_ulogic_vector; Y : STD_ULOGIC)
   			i_sign  => li_sign);
 
   		break_number(
-  			arg => r,
+  			input_value => r,
   			r_fract => rr_man,
   			i_fract => ri_man,
   			r_expon => rr_exp,
@@ -152,34 +152,34 @@ function find_leftmost (ARG : std_ulogic_vector; Y : STD_ULOGIC)
   			r_sign  => rr_sign,
   			i_sign  => ri_sign);
 
-  		shift_r := lr_exp - rr_exp;
-  		shift_i := li_exp - ri_exp;
+  		shift_r := to_integer(lr_exp - rr_exp);
+  		shift_i := to_integer(li_exp - ri_exp);
 
   		if shift_r < 0 then
-  			resrs_man := to_signed(lr_sign & shift_right (lr_man, to_integer(-shift_r)));
-  			resrc_man := to_signed(rr_sign & rr_man);
+  			resrs_man := signed(lr_sign & shift_right (lr_man, -shift_r));
+  			resrc_man := signed(rr_sign & rr_man);
   			resrt_exp := rr_exp;
   		elsif shift_r > 0 then
-  			resrs_man := to_signed(rr_sign & shift_right (rr_man, to_integer(shift_r)));
-  			resrc_man := to_signed(lr_sign & lr_man);
+  			resrs_man := signed(rr_sign & shift_right (rr_man, shift_r));
+  			resrc_man := signed(lr_sign & lr_man);
   			resrt_exp := lr_exp;
   		else
-  			resrs_man := to_signed(lr_sign & lr_man);
-  			resrc_man := to_signed(rr_sign & rr_man);
+  			resrs_man := signed(lr_sign & lr_man);
+  			resrc_man := signed(rr_sign & rr_man);
   			resrt_exp := rr_exp;
 		end if;
 
 		if shift_i < 0 then
-  			resis_man := to_signed(li_sign & shift_right (li_man, to_integer(-shift_i)));
-  			resic_man := to_signed(ri_sign & ri_man);
+  			resis_man := signed(li_sign & shift_right (li_man, -shift_i));
+  			resic_man := signed(ri_sign & ri_man);
   			resit_exp := ri_exp;
   		elsif shift_i > 0 then
-  			resis_man := to_signed(ri_sign & shift_right (ri_man, to_integer(shift_i)));
-  			resic_man := to_signed(li_sign & li_man);
+  			resis_man := signed(ri_sign & shift_right (ri_man, shift_i));
+  			resic_man := signed(li_sign & li_man);
   			resit_exp := li_exp;
   		else
-  			resis_man := to_signed(li_sign & li_man);
-  			resic_man := to_signed(ri_sign & ri_man);
+  			resis_man := signed(li_sign & li_man);
+  			resic_man := signed(ri_sign & ri_man);
   			resit_exp := li_exp;
 		end if;
 
@@ -189,8 +189,8 @@ function find_leftmost (ARG : std_ulogic_vector; Y : STD_ULOGIC)
 		pos_r := find_leftmost(resr_man, '1');
 		pos_i := find_leftmost(resi_man, '1');
 
-		expr_corr := pos_r - man_width-1;
-    expi_corr := pos_i - man_width-1;
+		expr_corr := to_unsigned(pos_r - 8, exp_width);
+    expi_corr := to_unsigned(pos_i - 8, exp_width);
 
     resr_exp := resrt_exp + expr_corr;
     resi_exp := resit_exp + expi_corr;
